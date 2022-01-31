3c698d6264db86998192057cabfdb7a396d6bceb
Argexitoo <argexitoo@gmail.com>
2022-01-31 17:07:14 +0100

    done

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,13 @@
+function Profileimage({image}) {
+    return (
+        <div>
+            <img
+        src={image}
+        className="profile"
+        alt="profile"
+      />
+        </div>
+    )
+}
+
+export default Profileimage
---
+++ src/components/Tweet.jsx
@@ -0,0 +1,30 @@
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
+
+function Tweet({
+  user,
+  message,
+  timestamp,
+}) {
+  return (
+    <div className="tweet">
+    <ProfileImage image={user.image} />
+      <div className="body">
+        <div className="top">
+          <User name={user.name} handle={user.handle} />
+          <Timestamp timestamp={timestamp} />
+        </div>
+          <Message message={message} />
+          <Actions />
+      </div>
+
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
